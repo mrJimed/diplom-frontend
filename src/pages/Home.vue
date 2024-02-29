9778a0d1b6dc41b8284114aeee80f07b7a00a4ef
Danil <danil0316@mail.ru>
2024-02-29 20:25:08 +0500

    update homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,22 +6,20 @@ import {
 } from '../services/summarizationService.js'
 
 const text = ref('')
-const topN = ref(3)
+const numberSentences = ref(3)
 const summarizationMethod = ref('extractive')
 
 async function onSummarizationClick() {
-  text.value = await extractiveSummarization(text.value, topN.value)
-  console.log(text.value)
+  if (summarizationMethod === 'extractive') {
+    text.value = await extractiveSummarization(text.value, numberSentences.value)
+  } else {
+    text.value = await abstractiveSummarization(text.value)
+  }
 }
 </script>
 
 <template>
-  <div>
-    <select v-model="summarizationMethod">
-      <option value="abstractive">Абстрактивная</option>
-      <option value="extractive">Экстрактивная</option>
-    </select>
-
+  <div class="mt-16">
     <form class="flex flex-col w-fit mx-auto gap-3" @submit.prevent="onSummarizationClick">
       <textarea
         class="border border-slate-300 bg-slate-100 resize-none outline-none rounded-md px-3 py-2 placeholder:italic"
@@ -30,13 +28,25 @@ async function onSummarizationClick() {
         placeholder="Введите текст..."
         v-model="text"
       ></textarea>
-      <input
-        class="border border-slate-300 bg-slate-100 rounded-md px-3 py-2"
-        type="number"
-        value="3"
-        min="3"
-        v-model="topN"
-      />
+
+      <div class="flex justify-between items-center">
+        <select v-model="summarizationMethod">
+          <option value="abstractive">Абстрактивная</option>
+          <option value="extractive">Экстрактивная</option>
+        </select>
+
+        <div class="flex items-center gap-4" v-if="summarizationMethod === 'extractive'">
+          <p>Кол-во предложений:</p>
+          <input
+            class="border border-slate-300 bg-slate-100 rounded-md px-2 py-2 w-24 text-center"
+            type="number"
+            value="3"
+            min="3"
+            v-model="numberSentences"
+          />
+        </div>
+      </div>
+
       <input
         class="cursor-pointer bg-green-600 rounded-md text-white py-2"
         type="submit"
