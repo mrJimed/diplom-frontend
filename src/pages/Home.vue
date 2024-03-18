9c24ff1e62b94eade80f59d7aaea02f2ab703098
Sanntan <gommaftegey04@mail.ru>
2024-03-18 19:32:07 +0300

    added send mail

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -28,19 +28,51 @@ async function onSummarizationClick() {
 </script>
 
 <template>
-  <div class="mt-16">
-    <form class="flex flex-col w-fit mx-auto gap-3" @submit.prevent="onSummarizationClick">
-      <div class="container mx-auto p-10">
-        <div class="flex flex-col items-center gap-4">
-          <div class="w-full max-w-sm">
-            <input type="file" id="file" @change="onFileChange" class="shadow-lg resize-none outline-none rounded-3xl px-3 py-2 bg-gray-100 placeholder-gray-900 text-gray-900 font-sans
-              dark:text-gray-400 dark:bg-gray-800 dark:placeholder-gray-400" />
-            <label for="file" class="cursor-pointer flex items-center justify-center w-full h-full text-gray-600">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
-              </svg>
-              <span class="ml-2 font-sans">Выберите или перенесите файл</span>
-            </label>
+  <div 
+  class="mt-10"
+  >
+    <form 
+    class="flex flex-col w-fit mx-auto gap-3" 
+    @submit.prevent="onSummarizationClick"
+    >
+      <div 
+      class="flex items-center justify-center w-full"
+      >
+        <label
+        v-if="!selectedFile"
+        for="dropzone-file" 
+        class="flex flex-col items-center justify-center w-96 h-64 shadow-xl rounded-3xl 
+        cursor-pointer bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-600
+        transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+        >
+          <div
+          class="flex flex-col items-center justify-center pt-5 pb-6"
+          >
+            <svg 
+            class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" 
+            aria-hidden="true" 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 20 16"
+            >
+              <path 
+              stroke="currentColor" 
+              stroke-linecap="round" 
+              stroke-linejoin="round" 
+              stroke-width="2" 
+              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+              />
+            </svg>
+            <p 
+            class="mb-2 text-sm text-gray-500 dark:text-gray-400"
+            >
+             <span>Click to upload</span>or drag and drop
+            </p>
+            <p
+            class="text-xs text-gray-500 dark:text-gray-400"
+            >
+             Any file types here
+            </p>
           </div>
           <input
           id="dropzone-file" 
@@ -81,10 +113,11 @@ async function onSummarizationClick() {
         </div>
       </div>
       <input
-        class="hover:bg-gray-900 transition hover:text-white mt-2 mb-7 cursor-pointer rounded-3xl bg-gray-100 text-gray-900 py-2 shadow-xl 
-        dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-400 dark:hover:text-white"
-        type="submit"
-        value="Отправить"
+      class="
+      hover:bg-gray-200 dark:hover:bg-gray-600 transition mt-2 mb-7 cursor-pointer rounded-3xl bg-gray-100 text-gray-500 py-2 shadow-xl 
+      dark:text-gray-400 dark:bg-gray-800"
+      type="submit"
+      value="Отправить"
       />
     </form>
 
@@ -141,14 +174,19 @@ async function onSummarizationClick() {
         </div>
       </div>
 
-      <div class="mt-5">
-        <!--... -->
-
-        <input type="checkbox" id="sendEmail" @change="onSendEmailChange">
-        <label for="sendEmail">После завершения отправить на email</label>
-
-        <!--... -->
+      <div class="flex justify-between items-center text-lg">
+        <div class="flex items-center gap-4 text-gray-500 dark:text-gray-400 font-sans">
+          <label class="flex items-center gap-2">
+            <input type="checkbox" v-model="sendEmailAfterCompletion" @change="toggleEmailInput">
+            Отправить на почту после завершения
+          </label>
+          <div v-if="showEmailInput" class="flex items-center gap-4 text-gray-500 dark:text-gray-400 font-sans">
+            <input class="outline-none bg-gray-100 px-2 py-2 w-80 text-center rounded-xl shadow-xl dark:bg-gray-800 font-sans" type="email" v-model="email" placeholder="Введите вашу электронную почту">
+            <button @click="sendEmail">Отправить</button>
+          </div>
+        </div>
       </div>
+
     </form>
   </div>
 
@@ -186,6 +224,7 @@ export default {
     sendEmail() {
       // логика отправки электронной почты
       console.log('Отправка на электронную почту:', this.email);
+
       this.sendEmailAfterCompletion = false;
       this.email = '';
       this.showEmailInput = false;
